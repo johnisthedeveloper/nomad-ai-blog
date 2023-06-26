70231b456a07d0fbddc7380e5be4ea59b0ff1d7c
JohnDeveloper <johnisthedeveloper@gmail.com>
2023-06-26 16:19:49 -0400

    push

---
+++ src/scripts/menu.js
@@ -0,0 +1,11 @@
+/*
+document.querySelector('.hamburger').addEventListener('click', () => {
+  document.querySelector('.nav-links').classList.toggle('expanded');
+});
+*/
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('expanded');
+  });
+});
